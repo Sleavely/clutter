7d3300853c938d273b6778dc200296e4546f3d47
Joakim Hedlund <contact@joakimhedlund.com>
2016-02-16 17:49:00 +0100

    Note to self

--- app/controllers/TvController.php
+++ app/controllers/TvController.php
@@ -5,6 +5,8 @@ class TvController extends BaseController {
 
   public function getIndex()
   {
+    //TODO: we could use TimelineController@getClutts to seed the page with some initial content
+    
     return View::make('timeline.tv');
   }
 }
