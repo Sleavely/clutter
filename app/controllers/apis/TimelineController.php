cf1eddfbcd9a4b9c269b2d663e6c014d39f9ce1d
Joakim Hedlund <contact@joakimhedlund.com>
2016-02-16 18:03:34 +0100

    Basic logic

--- app/controllers/apis/TimelineController.php
+++ app/controllers/apis/TimelineController.php
@@ -6,35 +6,50 @@ class TimelineController extends BaseController {
   public function __construct()
   {
     //TODO: run filters on all methods except basic GETs
+    //$this->beforeFilter('auth', array('except' => array('get', 'index', 'show')));
   }
 
-  public function getClutts($since = NULL)
+  /**
+   * Helper to determine whether the methods are called over HTTP or internally.
+   */
+  private function isInternal()
   {
-    $clutts = Clutt::byFollowed();
+    return Request::segment(1) != 'api';
+  }
 
-    //TODO: this makes no sense to put here. when will $since be supplied?
-    if(is_string($since))
-    {
-      $since = Carbon::parse($since);
-    }
-    if($since instanceof Carbon)
-    {
-      $clutts->sinceLastFetched();
-    }
+  public function getClutts()
+  {
+    $clutts = Clutt::sinceLastFetched();
+    Session::set('last-fetched', Carbon::now());
 
     return $clutts->get();
   }
 
   public function postClutter()
   {
-    //TODO: get clutter input.
+    $message = Input::get('message', '');
 
-    // AJAX gets the clutt in response
-    if (Request::ajax())
+    // Validate input
+    $rules = array(
+      'message' => 'required|max:140'
+    );
+    $validator = Validator::make(Input::all(), $rules);
+    if ($validator->fails())
+    {
+      $errors = $validator->errors()->all();
+      return Redirect::action('PostController@getIndex')->withInput()->with('errors', $errors);
+    }
+
+    // If we got this far we probably did something right :)
+    $clutt = new Clutt;
+    //TODO: save to database lol.
+
+    // AJAX gets the whole clutt in response. Same if some other controller calls the method.
+    if (Request::ajax() || $this->isInternal())
     {
       return $clutt;
     }
-    // HTTP gets redirect to timeline
-    return Redirect::action('TimelineController@getIndex');
+    // HTTP gets redirect back to form
+    return Redirect::action('PostController@getIndex')->with('created-clutt', $clutt->id);
   }
 }
