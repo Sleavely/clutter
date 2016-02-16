180d337a30b93b5e827daaf788f8520aab56932a
Joakim Hedlund <contact@joakimhedlund.com>
2016-02-16 15:41:06 +0100

    Route to the controllers

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,25 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	// In case we every change our mind what's supposed to be on the front page.
+	return Redirect::action('PostController@getIndex');
+});
+
+// Front-end controllers.
+// The first ::get is to force the action() method to not generate /tv/index
+Route::get('/tv', 'TvController@getIndex');
+Route::controller('tv', 'TvController');
+
+Route::get('/post', 'PostController@getIndex');
+Route::controller('post', 'PostController');
+
+// APIs.
+// A separate copy of this application could be deployed with only these routes to only
+// handle API calls, while letting some other server handle other, more front-endy requests.
+// In that theoretical separation you'd convert the frontend application's API controllers to query
+// the actual APIs, since the methods are also called directly by the front-end controllers.
+Route::group(array('prefix' => 'api'), function()
+{
+	Route::get('/timeline', 'TimelineController@getIndex');
+	Route::controller('timeline', 'TimelineController');
 });
